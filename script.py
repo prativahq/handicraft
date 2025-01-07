0d112a94fa171707d23233388a159f978aea7951
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2025-01-07 19:39:09 +0530

    change

--- script.py
+++ script.py
@@ -18,7 +18,13 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 
-tables = ["7903_wc_customer_lookup", "7903_wc_order_stats", "7903_wc_order_product_lookup", "7903_posts", "7903_term_taxonomy"]
+tables = [
+    "7903_wc_customer_lookup",
+    "7903_wc_order_stats",
+    "7903_wc_order_product_lookup",
+    "7903_posts",
+    "7903_term_taxonomy",
+]
 
 
 # Database configuration (store these in environment variables for security)
@@ -30,6 +36,52 @@ SALESFORCE_URI = os.getenv("SALESFORCE_URI")
 SALESFORCE_API_KEY = os.getenv("SALESFORCE_API_KEY")
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
+states = {
+    "AL": "Alabama",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DC": "District of Columbia",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Ilinois",
+    "IN": "Indiana",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OR": "oregon",
+    "PA": "Pennsytvania",
+    "RI": "Rhode Island",
+    "SC": "South carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "VA": "vrgfmia",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "west Virginia",
+    "WY": "Wyornng",
+}
+
 
 def send_email(content):
     try:
@@ -40,7 +92,7 @@ def send_email(content):
         content = Content("text/json", json.dumps(content))
         subject = "HC Notification"
         mail = Mail(from_email, to_email, subject, content, is_multiple=len(emails) > 1)
-        
+
         response = sg.client.mail.send.post(request_body=mail.get())
         logging.info(response)
         return response
@@ -61,7 +113,7 @@ def upload_data(df, table):
                     "object": table,
                     "contentType": "CSV",
                     "operation": "insert",
-                    "lineEnding": "CRLF" if sys.platform.startswith('win') else "LF",
+                    "lineEnding": "CRLF" if sys.platform.startswith("win") else "LF",
                 }
             ),
             "application/json",
@@ -81,7 +133,7 @@ def upload_data(df, table):
     if res.status_code != 200:
         logging.info(res.text)
         return
-    
+
     res = res.json()
     logging.info(res["id"])
 
@@ -130,7 +182,7 @@ def upload_data(df, table):
         logging.info(result)
         # send_email(result)
         return result
-    
+
 
 def fetch_changes(table_name):
     try:
@@ -190,38 +242,57 @@ def process_and_save_members(changes):
     df = pd.DataFrame(results)
     logging.info(f"Processing {len(df)} records")
     logging.info(df)
-    
 
-    df = df[["first_name", "last_name", "email", "city", "customer_id", "phone", "state", "postcode", "membership_plan"]]
-    df.rename(columns={"customer_id": "Member_ID__c", 
-                    "first_name": "First_Name__c", 
-                    "last_name": "Last_Name__c", 
-                    "email": "Email__c", 
-                    "city": "City__c", 
-                    "phone": "Primary_Phone__c", 
-                    "state": "State__c", 
-                    "postcode": "Zipcode__c", 
-                    "membership_plan": "Membership_Plan__c"}, 
-                    inplace=True, errors="ignore")
+    df = df[
+        [
+            "first_name",
+            "last_name",
+            "email",
+            "city",
+            "customer_id",
+            "phone",
+            "state",
+            "postcode",
+            "membership_plan",
+        ]
+    ]
+    df.rename(
+        columns={
+            "customer_id": "Member_ID__c",
+            "first_name": "First_Name__c",
+            "last_name": "Last_Name__c",
+            "email": "Email__c",
+            "city": "City__c",
+            "phone": "Primary_Phone__c",
+            "state": "State__c",
+            "postcode": "Zipcode__c",
+            "membership_plan": "Membership_Plan__c",
+        },
+        inplace=True,
+        errors="ignore",
+    )
     df["Source__c"] = f"wpdatabridge - {datetime.now().strftime(r'%Y-%m-%d')}"
     df["Member_Status__c"] = "Active"
-    df["Membership_Plan__c"] = df["Membership_Plan__c"].map({
-        411: "Active Member",
-        6510: "Active Mernber - Emeritus",
-        6511: "Active Member - Out of Town",
-        6514: "Active Mernber - Senior",
-        7478: "Active Member - Staff",
-        413: "Guest",
-        412: "Guests with Provisional Status (GPS)",
-        7472: "Leave of Absence (LOA)",
-        7385: "Resigned - Bad",
-        7384: "Resigned - Good",
-        7386: "Deceased",
-        None: "Active Member"
-    })
+    df["State__c"] = df["State__c"].map(states)
+    df["Membership_Plan__c"] = df["Membership_Plan__c"].map(
+        {
+            411: "Active Member",
+            6510: "Active Mernber - Emeritus",
+            6511: "Active Member - Out of Town",
+            6514: "Active Mernber - Senior",
+            7478: "Active Member - Staff",
+            413: "Guest",
+            412: "Guests with Provisional Status (GPS)",
+            7472: "Leave of Absence (LOA)",
+            7385: "Resigned - Bad",
+            7384: "Resigned - Good",
+            7386: "Deceased",
+            None: "Active Member",
+        }
+    )
     df = df.fillna("")
     # df["Membership_Plan__c"] = "Active Member"
-    
+
     df = df.map(convert)
     upload_data(df, "HC_Member__c")
 
@@ -243,22 +314,43 @@ def process_and_save_orders(changes):
     logging.info(f"Processing {len(df)} records")
 
     #  Customer_Note__c, Order_Notes__c, Payment_Method__c, Reference__c, Refund_Items__c, Transaction_ID__c
-    df = df[["date_completed", "customer_id", "status", "order_id", "date_created", "parent_id" ]]
-    df.rename(columns={"date_completed": "Completed_Date__c", 
-                        "customer_id": "Member_ID__c", 
-                        "status": "Order_Status__c", 
-                        "order_id": "Order_Number__c", 
-                        "date_created": "Order_Date__c" }, inplace=True, errors="ignore")
+    df = df[
+        [
+            "date_completed",
+            "customer_id",
+            "status",
+            "order_id",
+            "date_created",
+            "parent_id",
+        ]
+    ]
+    df.rename(
+        columns={
+            "date_completed": "Completed_Date__c",
+            "customer_id": "Member_ID__c",
+            "status": "Order_Status__c",
+            "order_id": "Order_Number__c",
+            "date_created": "Order_Date__c",
+        },
+        inplace=True,
+        errors="ignore",
+    )
     df["Source__c"] = f"wpdatabridge - {datetime.now().strftime(r'%Y-%m-%d')}"
-    df["Order_Status__c"] = df["Order_Status__c"].map({"wc-refunded": "refunded",
-                            "wc-processing": "processing",
-                            "wc-completed": "completed",
-                            "wc-on-hold": "on-hold",
-                            "wc-cancelled": "cancelled"})
-    df["Refund_Items__c"] = np.where((df["parent_id"] != 0) & (df["Order_Status__c"] == "refunded"), "Refunded", "")
-    
+    df["Order_Status__c"] = df["Order_Status__c"].map(
+        {
+            "wc-refunded": "refunded",
+            "wc-processing": "processing",
+            "wc-completed": "completed",
+            "wc-on-hold": "on-hold",
+            "wc-cancelled": "cancelled",
+        }
+    )
+    df["Refund_Items__c"] = np.where(
+        (df["parent_id"] != 0) & (df["Order_Status__c"] == "refunded"), "Refunded", ""
+    )
+
     df.drop(columns=["parent_id"], inplace=True)
-    
+
     df = df.fillna("")
     df = df.map(convert)
 
@@ -281,12 +373,13 @@ def process_and_save_order_items(changes):
     df = pd.DataFrame(results)
     logging.info(f"Processing {len(df)} records")
 
-    df = df[["product_quantity" ]]
-    df.rename(columns={"product_quantity": "Item_Quantity__c" }, inplace=True, errors="ignore")
+    df = df[["product_quantity"]]
+    df.rename(
+        columns={"product_quantity": "Item_Quantity__c"}, inplace=True, errors="ignore"
+    )
     df["Ken_s_Field__c"] = False
     df["Source__c"] = f"wpdatabridge - {datetime.now().strftime(r'%Y-%m-%d')}"
 
-    
     df = df.fillna("")
     df = df.map(convert)
 
@@ -309,59 +402,91 @@ def process_and_save_product(changes):
     mycursor.execute(query, ids)
     results = mycursor.fetchall()
 
-    mycursor.execute("SELECT DISTINCT(object_id), term_taxonomy_id as cat FROM `7903_term_relationships` WHERE term_taxonomy_id IN (23, 192, 256, 27, 111, 42, 64);")
+    mycursor.execute(
+        "SELECT DISTINCT(object_id), term_taxonomy_id as cat FROM `7903_term_relationships` WHERE term_taxonomy_id IN (23, 192, 256, 27, 111, 42, 64);"
+    )
     categories = mycursor.fetchall()
 
-    mycursor.execute("SELECT DISTINCT(object_id), term_taxonomy_id as cat FROM `7903_term_relationships` WHERE term_taxonomy_id IN (31, 32, 37, 34, 40, 48);")
+    mycursor.execute(
+        "SELECT DISTINCT(object_id), term_taxonomy_id as cat FROM `7903_term_relationships` WHERE term_taxonomy_id IN (31, 32, 37, 34, 40, 48);"
+    )
     day_of_week = mycursor.fetchall()
 
     mydb.close()  # Close the connection as soon as we're done
 
     if results is None or len(results) == 0:
         return
-    
+
     df = pd.DataFrame(results)
     categories = pd.DataFrame(categories)
     day_of_week = pd.DataFrame(day_of_week)
     logging.info(f"Processing {len(df)} records")
 
-    df = df[["ID", "post_title", "post_date", "guid", "max_price" ]]
+    df = df[["ID", "post_title", "post_date", "guid", "max_price"]]
     df = df.map(convert)
-    df.rename(columns={"ID": "Product_Identifier__c", "post_title": "Product_Name__c", "guid": "Product_Page_URL__c", "max_price": "Regular_Price__c" }, inplace=True, errors="ignore")
+    df.rename(
+        columns={
+            "ID": "Product_Identifier__c",
+            "post_title": "Product_Name__c",
+            "guid": "Product_Page_URL__c",
+            "max_price": "Regular_Price__c",
+        },
+        inplace=True,
+        errors="ignore",
+    )
     df["Did_Not_Run__c"] = False
-    df["Post_Date__c"] = pd.to_datetime(df["post_date"]).dt.strftime('%Y-%m-%d')
-    df["Time__c"] = pd.to_datetime(df["post_date"]).dt.strftime('%H:%M:%S')
-    df["Trimester__c"] = df["post_date"].dt.month.map(lambda x: "Spring" if x in [ 4, 5, 6, 7] else "Fall" if x in [8, 9, 10, 11] else "Winter")
+    df["Post_Date__c"] = pd.to_datetime(df["post_date"]).dt.strftime("%Y-%m-%d")
+    df["Time__c"] = pd.to_datetime(df["post_date"]).dt.strftime("%H:%M:%S")
+    df["Trimester__c"] = df["post_date"].dt.month.map(
+        lambda x: "Spring"
+        if x in [4, 5, 6, 7]
+        else "Fall"
+        if x in [8, 9, 10, 11]
+        else "Winter"
+    )
     df["Year__c"] = df["post_date"].dt.year
     df["Source__c"] = f"wpdatabridge - {datetime.now().strftime(r'%Y-%m-%d')}"
-    df["Category__c"] = df["ID"].apply(
-        lambda x: categories.loc[categories.object_id == x, "cat"].iloc[0] 
-        if x in categories.object_id.values else None).apply({
-            23: "Classes",
-            192: "Clubhouse Only",
-            256: "Event",
-            27: "Membership",
-            111: "Open Studios",
-            42: "Transportation",
-            64: "Workshops"
-        })
-    df["Day_of_Week__c"] = df["ID"].apply(
-        lambda x: day_of_week.loc[day_of_week.object_id == x, "cat"].iloc[0]
-        if x in day_of_week.object_id.values else None).apply({
-            31: "Monday",
-            32: "Tuesday",
-            37: "Wednesday",
-            34: "Thursday",
-            40: "Friday",
-            48: "Saturday"
-        })
-    
-    
+    df["Category__c"] = (
+        df["ID"]
+        .apply(
+            lambda x: categories.loc[categories.object_id == x, "cat"].iloc[0]
+            if x in categories.object_id.values
+            else None
+        )
+        .apply(
+            {
+                23: "Classes",
+                192: "Clubhouse Only",
+                256: "Event",
+                27: "Membership",
+                111: "Open Studios",
+                42: "Transportation",
+                64: "Workshops",
+            }
+        )
+    )
+    df["Day_of_Week__c"] = (
+        df["ID"]
+        .apply(
+            lambda x: day_of_week.loc[day_of_week.object_id == x, "cat"].iloc[0]
+            if x in day_of_week.object_id.values
+            else None
+        )
+        .apply(
+            {
+                31: "Monday",
+                32: "Tuesday",
+                37: "Wednesday",
+                34: "Thursday",
+                40: "Friday",
+                48: "Saturday",
+            }
+        )
+    )
+
     df = df.fillna("")
     df = df.map(convert)
     upload_data(df, "HC_Product__c")
-
-
 
 
 def process_and_save_teachers(changes):
@@ -380,14 +505,15 @@ def process_and_save_teachers(changes):
     df = pd.DataFrame(results)
     logging.info(f"Processing {len(df)} records")
 
-    df = df[["name" ]]
-    df.rename(columns={"name": "Name" }, inplace=True, errors="ignore")
+    df = df[["name"]]
+    df.rename(columns={"name": "Name"}, inplace=True, errors="ignore")
     df["Also_a_Member__c"] = False
 
     df = df.fillna("")
     df = df.map(convert)
 
     upload_data(df, "HC_Teacher__c")
+
 
 # def update_processed_flags(changes):
 #     try:
@@ -409,15 +535,13 @@ def process_and_save_teachers(changes):
 #         logging.info(f"Database error while updating processed flags: {err}")
 
 
-
-
 if __name__ == "__main__":
     for table in tables:
         changes_data = fetch_changes(table)
         if changes_data is None:
             continue
         logging.info(f"Processing {table}")
-        if table == "7903_wc_customer_lookup": 
+        if table == "7903_wc_customer_lookup":
             process_and_save_members(changes_data)
         if table == "7903_wc_order_stats":
             process_and_save_orders(changes_data)
@@ -428,7 +552,6 @@ if __name__ == "__main__":
         if table == "7903_posts":
             process_and_save_product(changes_data)
 
-    
-# order_item - 35  "Monday Knitting Circle with Kay Mehls"
-# product - 762 
 
+# order_item - 35  "Monday Knitting Circle with Kay Mehls"
+# product - 762
